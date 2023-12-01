1bfff5a31ad45a4749a4ecb4296f313e7e024ba3
punchihewa <navishkapunchihewa66@gmail.com>
2023-12-01 11:42:29 +0530

    first commit

---
+++ src/main/java/lk/ijse/gdse/hello/api/Hello_Servelet.java
@@ -0,0 +1,15 @@
+package lk.ijse.gdse.hello.api;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Hello_Servelet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Incoming request: Hello Servlet" +req.getRemoteAddr());
+        resp.getWriter().println("Hello I From the Hello Server 1");
+    }
+}
