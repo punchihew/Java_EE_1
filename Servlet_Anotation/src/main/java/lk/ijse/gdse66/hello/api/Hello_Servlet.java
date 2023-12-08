8e17ebd0e60850e94c2ee365ea42835ee551ec1c
punchihewa <navishkapunchihewa66@gmail.com>
2023-12-08 11:10:37 +0530

    Servlet annotation

--- Servlet_Anotation/src/main/java/lk/ijse/gdse66/hello/api/Hello_Servlet.java
+++ Servlet_Anotation/src/main/java/lk/ijse/gdse66/hello/api/Hello_Servlet.java
@@ -1,15 +1,17 @@
 package lk.ijse.gdse66.hello.api;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class Hello_Servlet extends HttpServlet {
+@WebServlet(name = "Hello_Servlet" , value = "/")
+public class  Hello_Servlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-
+        System.out.println("Printed...!!");
     }
 }
