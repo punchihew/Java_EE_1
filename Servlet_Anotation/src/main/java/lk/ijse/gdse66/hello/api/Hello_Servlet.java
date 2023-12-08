c7038902da642d4fd39a926746d5ccd1c9a16a49
punchihewa <navishkapunchihewa66@gmail.com>
2023-12-08 10:56:09 +0530

    Servlet life cycle

--- Servlet_Anotation/src/main/java/lk/ijse/gdse66/hello/api/Hello_Servlet.java
+++ Servlet_Anotation/src/main/java/lk/ijse/gdse66/hello/api/Hello_Servlet.java
@@ -1,4 +1,15 @@
 package lk.ijse.gdse66.hello.api;
 
-public class Hello_Servlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Hello_Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+    }
 }
