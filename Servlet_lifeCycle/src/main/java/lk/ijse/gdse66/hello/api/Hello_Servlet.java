b23d9e94fb0e1ec9647028dd4aa19cd558678609
punchihewa <navishkapunchihewa66@gmail.com>
2023-12-08 09:49:38 +0530

    context update

--- Servlet_lifeCycle/src/main/java/lk/ijse/gdse66/hello/api/Hello_Servlet.java
+++ Servlet_lifeCycle/src/main/java/lk/ijse/gdse66/hello/api/Hello_Servlet.java
@@ -11,6 +11,7 @@ public class Hello_Servlet  extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.getWriter().println("Hello Servlet 1");
+        System.out.println("Hello");
 
     }
 }
