c1726b660200bfe4e171786adf3af67dc5be8c77
punchihewa <navishkapunchihewa66@gmail.com>
2023-12-08 11:12:35 +0530

    Servlet annotation

--- Servlet_Anotation/src/main/java/lk/ijse/gdse66/hello/api/Hello_Servlet.java
+++ Servlet_Anotation/src/main/java/lk/ijse/gdse66/hello/api/Hello_Servlet.java
@@ -12,6 +12,6 @@ public class  Hello_Servlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Printed...!!");
+        System.out.println("Goget...!!");
     }
 }
