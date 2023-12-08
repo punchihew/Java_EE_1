50865330abec1c6e07707040984496047b9f630a
punchihewa <navishkapunchihewa66@gmail.com>
2023-12-08 11:34:05 +0530

    Servlet annotation

--- Servlet_Database/src/main/java/lk/ijse/gdse66/hello/api/Hello_Servlet.java
+++ Servlet_Database/src/main/java/lk/ijse/gdse66/hello/api/Hello_Servlet.java
@@ -1,7 +1,24 @@
 package lk.ijse.gdse66.hello.api;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebServlet(name = "Hello_Servlet" , value = "/" , loadOnStartup = 1, initParams = {
+        @WebInitParam(name ="username" , value = "root"),
+        @WebInitParam(name = "password" , value = "1234"),
+        @WebInitParam(name = "url" , value = "jdbc:mysql://localhost:3306/gdse66_hello")
 
+})
 
-public class Hello_Servlet extends {
+public class  Hello_Servlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Goget...!!");
+    }
 }
