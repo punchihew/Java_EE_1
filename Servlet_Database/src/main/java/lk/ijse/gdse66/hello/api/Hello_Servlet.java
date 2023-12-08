927f581833d55c41b5f09d15b4dc9dc1f55ffd1d
punchihewa <navishkapunchihewa66@gmail.com>
2023-12-08 11:58:15 +0530

    Servlet detabase

--- Servlet_Database/src/main/java/lk/ijse/gdse66/hello/api/Hello_Servlet.java
+++ Servlet_Database/src/main/java/lk/ijse/gdse66/hello/api/Hello_Servlet.java
@@ -1,5 +1,6 @@
 package lk.ijse.gdse66.hello.api;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
@@ -7,6 +8,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @WebServlet(name = "Hello_Servlet" , value = "/" , loadOnStartup = 1, initParams = {
         @WebInitParam(name ="username" , value = "root"),
@@ -16,9 +21,39 @@ import java.io.IOException;
 })
 
 public class  Hello_Servlet extends HttpServlet {
+    private String username;
+    private String password;
+    private String url;
+
+
+    //connecting eka hadagena dunne attrubete wala vallue eka illgannwa
+    @Override
+    public void init() throws ServletException {
+        ServletConfig sc = getServletConfig();
+        String username =  sc.getInitParameter("username");
+        String password = sc.getInitParameter("password");
+        String url = sc.getInitParameter("url");
+
+
+    }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Goget...!!");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Connection connection = null;
+        try {
+            String id = req.getParameter("id");
+            String name = req.getParameter("name");
+            String address = req.getParameter("address");
+
+            
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            DriverManager.getConnection(url,username,password);
+//            PreparedStatement stn = connection.prepareStatement("INSERT INTO customer(id,name,address) VALUES (?,?,?)");
+//            stn.setString(1,id);
+//            stn.setString(2,name);
+//            stn.setString((3,addres));
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
