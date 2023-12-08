49d79b104c4ee9f7be6f2c6ed5656c135ef1c248
punchihewa <navishkapunchihewa66@gmail.com>
2023-12-08 10:41:37 +0530

    Servlet life cycle

--- Servlet_lifeCycle/src/main/java/lk/ijse/gdse66/hello/api/Hello_Servlet_2.java
+++ Servlet_lifeCycle/src/main/java/lk/ijse/gdse66/hello/api/Hello_Servlet_2.java
@@ -9,19 +9,7 @@ import java.io.IOException;
 
 public class Hello_Servlet_2 extends HttpServlet {
 
-    static{
-        System.out.println("Servlet is being initialized");
-    }
-    public Hello_Servlet_2(){
-        System.out.println("Servlet(): Constructomer ");
-    }
-
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        System.out.println("init(ServletConfig)");
-        super.init(config);
-    }
-
+   
     @Override
     public void init() throws ServletException {
         System.out.println("int()");
