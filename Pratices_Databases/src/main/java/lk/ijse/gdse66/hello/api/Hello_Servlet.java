dc8d19a20a88603638d2affbab1dc1b2c183080e
punchihewa <navishkapunchihewa66@gmail.com>
2023-12-09 12:00:19 +0530

    pratice update database

---
+++ Pratices_Databases/src/main/java/lk/ijse/gdse66/hello/api/Hello_Servlet.java
@@ -0,0 +1,16 @@
+package lk.ijse.gdse66.hello.api;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Hello_Servlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        
+    }
+}
