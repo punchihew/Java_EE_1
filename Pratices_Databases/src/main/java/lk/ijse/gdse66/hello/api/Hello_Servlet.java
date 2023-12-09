dcdddef7e98ef53be406e9e35daf2a7e49b2723b
punchihewa <navishkapunchihewa66@gmail.com>
2023-12-09 12:04:39 +0530

    pratice update database

--- Pratices_Databases/src/main/java/lk/ijse/gdse66/hello/api/Hello_Servlet.java
+++ Pratices_Databases/src/main/java/lk/ijse/gdse66/hello/api/Hello_Servlet.java
@@ -1,16 +1,20 @@
 package lk.ijse.gdse66.hello.api;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+//using annotation web app
+@WebServlet(name = "Hello_Servlet" , value = "MyServlet" ,loadOnStartup = 1 )
+
 public class Hello_Servlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+    
 
     }
 }
