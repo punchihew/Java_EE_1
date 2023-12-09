8e4494480a6e0c9954082fed01de4117b4cd1dca
punchihewa <navishkapunchihewa66@gmail.com>
2023-12-09 12:28:40 +0530

    pratice update

--- Pratices_Databases/src/main/java/lk/ijse/gdse66/hello/api/Hello_Servlet.java
+++ Pratices_Databases/src/main/java/lk/ijse/gdse66/hello/api/Hello_Servlet.java
@@ -1,6 +1,7 @@
 package lk.ijse.gdse66.hello.api;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,7 +10,9 @@ import java.io.IOException;
 
 //using annotation web app
 @WebServlet(name = "Hello_Servlet" , value = "/" ,loadOnStartup = 1 , initParams = {
-         
+         @WebInitParam(name = "usesrname" , value = "root"),
+         @WebInitParam(name = "password" , value = "1234"),
+         @WebInitParam(name = "url" , value = "jdbc:mysql://localhost:3306/gdse66_hello")
 })
 
 public class Hello_Servlet extends HttpServlet {
