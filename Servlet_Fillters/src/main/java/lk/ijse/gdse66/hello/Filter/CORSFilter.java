8bd3768c669ab4bb4e4b6d17ec741281d1ba94d3
punchihewa <navishkapunchihewa66@gmail.com>
2024-01-19 12:35:46 +0530

    Servlet Fillters

--- Servlet_Fillters/src/main/java/lk/ijse/gdse66/hello/Filter/CORSFilter.java
+++ Servlet_Fillters/src/main/java/lk/ijse/gdse66/hello/Filter/CORSFilter.java
@@ -1,6 +1,17 @@
 package lk.ijse.gdse66.hello.Filter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebFilter(filterName = "CORSFilter",urlPatterns = "/customers")
 public class CORSFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+    }
 }
