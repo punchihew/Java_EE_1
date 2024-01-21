4d8b1eaafe2cd441093336df8b0f8c2f9a05fb6b
punchihewa <navishkapunchihewa66@gmail.com>
2024-01-21 10:51:15 +0530

    java ee final 1.0.0

--- Java-EE-Final/src/main/java/lk/ijse/gdse66/hello/api/My_Servlet.java
+++ Java-EE-Final/src/main/java/lk/ijse/gdse66/hello/api/My_Servlet.java
@@ -1,4 +1,16 @@
 package lk.ijse.gdse66.hello.api;
 
-public class My_Servlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class My_Servlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
