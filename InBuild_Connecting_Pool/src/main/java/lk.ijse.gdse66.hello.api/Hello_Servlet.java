e718f5e3ca591372096a051c9d21fbce80c3a4c3
punchihewa <navishkapunchihewa66@gmail.com>
2024-01-26 10:03:49 +0530

    inbuild-cp 1.0.0

--- InBuild_Connecting_Pool/src/main/java/lk.ijse.gdse66.hello.api/Hello_Servlet.java
+++ InBuild_Connecting_Pool/src/main/java/lk.ijse.gdse66.hello.api/Hello_Servlet.java
@@ -6,12 +6,32 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @WebServlet(urlPatterns = "/customer")
 public class Hello_Servlet extends HttpServlet {
+
+   @Override
+   public void init() throws ServletException {
+      try {
+         InitialContext ic = new InitialContext();
+         DataSource pool = (DataSource) ic.lookup("java:/comp/env/jdbc/pos");
+         Connection connection = pool.getConnection();
+         System.out.println(connection);
+      } catch (NamingException | SQLException e) {
+         throw new RuntimeException();
+      }
+   }
+
    @Override
    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-      System.out.println("Hello");
+
    }
+
 }
