2f2eb163804f34f387c42002ee748493ae4a0a62
punchihewa <navishkapunchihewa66@gmail.com>
2024-01-31 10:50:39 +0530

    itemDao

---
+++ Java-EE-Final/Java-EE-Final/src/main/java/lk/ijse/gdse66/hello/dao/custom/ItemDAO.java
@@ -0,0 +1,8 @@
+package lk.ijse.gdse66.hello.dao.custom;
+
+import lk.ijse.gdse66.hello.dao.CrudDao;
+import lk.ijse.gdse66.hello.entity.Item;
+
+public interface ItemDAO extends CrudDao<Item> {
+
+}
