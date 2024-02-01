6d6dc0b0bce82206cdef50d4b7473aa0d2994b30
punchihewa <navishkapunchihewa66@gmail.com>
2024-02-01 12:09:15 +0530

    update

---
+++ Java-EE-Final/Java-EE-Final/src/main/java/lk/ijse/gdse66/hello/dto/OrderDetailsDTO.java
@@ -0,0 +1,62 @@
+package lk.ijse.gdse66.hello.dto;
+
+import java.io.Serializable;
+
+public class OrderDetailsDTO implements Serializable {
+    private String itemCode;
+    private String orderID;
+    private int qty;
+    private double unitPrice;
+
+    public OrderDetailsDTO(String itemCode, String orderID, int qty, double unitPrice) {
+        this.itemCode = itemCode;
+        this.orderID = orderID;
+        this.qty = qty;
+        this.unitPrice = unitPrice;
+    }
+
+    public OrderDetailsDTO() {
+    }
+
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+
+    public String getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(String orderID) {
+        this.orderID = orderID;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDetail{" +
+                "itemCode='" + itemCode + '\'' +
+                ", orderID='" + orderID + '\'' +
+                ", qty=" + qty +
+                ", unitPrice=" + unitPrice +
+                '}';
+    }
+}
